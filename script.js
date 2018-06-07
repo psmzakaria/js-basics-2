88b45f49dca0c137fef66a3e6d143d998a94259b
psmzakaria <psmzakaria@gmail.com>
2018-06-07 17:29:17 +0800

    New edit

---
+++ script.js
@@ -0,0 +1,38 @@
+// const dog = {
+//   name: "Pupper",
+
+//   bark() {
+//     console.log("woof, I'm ", this.name);
+//   }
+// };
+// const person ={
+
+// firstName:"John",
+// lastName:"Meow",
+// birthYear:100,
+
+// name(){
+
+//     return (this.firstName + this.lastName);
+// },
+
+//  ageThisYear(){
+
+// return (person.birthYear);
+
+//  },
+
+// }
+
+
+const someString = "AAAAAA";
+somestring = {
+
+length:200,
+
+}
+
+
+
+
+var decimalNumber = 30;
